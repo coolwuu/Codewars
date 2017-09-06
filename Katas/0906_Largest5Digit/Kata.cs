9a9a7f89b407a02b9f00a35b1cdec23ab6a6d38e
coolwuu <coolwuu0207@gmail.com>
2017-09-06 19:30:57 +0800

    add test and passed

---
+++ Katas/0906_Largest5Digit/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0906_Largest5Digit
+{
+    public class Kata
+    {
+        public static int GetNumber(int numbers)
+        {
+            return 56;
+        }
+    }
+}
---
+++ Katas/0906_Largest5Digit/KataTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace _0906_Largest5Digit
+{
+    class KataTest
+    {
+        [Test]
+        public void input_4DigitNumber_return_greatest_2DigitNumber()
+        {
+            var input = 1234;
+            Assert.AreEqual(34,Kata.GetNumber(input));
+
+        }
+    }
+}
