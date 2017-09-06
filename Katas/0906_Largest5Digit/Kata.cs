f9c33499e8d5883efc93081934250d22f6d3c817
coolwuu <coolwuu0207@gmail.com>
2017-09-06 20:36:32 +0800

    passed

--- Katas/0906_Largest5Digit/Kata.cs
+++ Katas/0906_Largest5Digit/Kata.cs
@@ -8,9 +8,15 @@ namespace _0906_Largest5Digit
 {
     public class Kata
     {
-        public static int GetNumber(int numbers)
+        public static int GetNumber(string numbers)
         {
-            return 56;
+            List<int> twoDigitNumberList = new List<int>();
+            for (var i = 0; i < numbers.Length-4; i++)
+            {
+                twoDigitNumberList.Add(Convert.ToInt32(numbers.Substring(i, 5)));
+            }
+            return twoDigitNumberList.Select(x => x).Max();
+
         }
     }
 }
