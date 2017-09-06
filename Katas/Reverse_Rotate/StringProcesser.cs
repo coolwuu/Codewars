d214535133410fbc2ae994be5728ba696a1d00dc
coolwuu <coolwuu0207@gmail.com>
2017-09-07 00:38:41 +0800

    pass 3rd test

--- Katas/Reverse_Rotate/StringProcesser.cs
+++ Katas/Reverse_Rotate/StringProcesser.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _0906_Reverse_Rotate
 {
     public class StringProcesser
     {
-        public static string RevRot(string numbers, int size)
+        public static string RevRot(string number, int size)
         {
-            if(size <= 0)
+            if (size <= 0)
                 return string.Empty;
-            return numbers;
+
+            var result=string.Empty;
+            var numbers = Regex.Matches(number, @"\d{2}");
+;            foreach (Match num in numbers)
+            {
+                char[] digitArray = num.Value.ToCharArray();
+                if (num.Value.Length >= size)
+                {
+                    if (int.Parse(num.Value) % 2 == 0)
+                    {
+                        Array.Reverse(digitArray);
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+                result += new string(digitArray);
+            }
+            return result;
         }
     }
 }
