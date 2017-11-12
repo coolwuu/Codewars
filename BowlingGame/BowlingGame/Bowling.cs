7699aa9215d7e268f8f3909a1f8e4208e52d9300
coolwuu <coolwuu0207@gmail.com>
2017-11-12 21:33:01 +0800

    add fail test :: all frames got 9 pins should return 90 and rename test methods

--- BowlingGame/BowlingGame/Bowling.cs
+++ BowlingGame/BowlingGame/Bowling.cs
@@ -7,18 +7,22 @@ namespace Bowling
         readonly BowlingGame _bowlingGame = new BowlingGame();
 
         [Test]
-        public void all_zero_pins_should_return_0()
+        public void all_frames_got_zero_pins_should_return_0()
         {
             ScoreShouldBe(0,"0- 0- 0- 0- 0- 0- 0- 0- 0- 0-");
         }
 
         [Test]
-        public void all_one_pins_should_return_10()
+        public void all_frames_got_one_pins_should_return_10()
         {
             ScoreShouldBe(10, "1- 1- 1- 1- 1- 1- 1- 1- 1- 1-");
         }
 
-
+        [Test]
+        public void all_frames_got_9_pins_should_return_90()
+        {
+            ScoreShouldBe(90, "18 18 81 63 36 45 54 9- 09 18");
+        }
 
         private void ScoreShouldBe(int expected,string playerResult)
         {
