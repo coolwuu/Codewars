c62103e648ea6c8dfd661505410c2ee0521a008d
coolwuu <coolwuu0207@gmail.com>
2017-11-12 21:20:50 +0800

    add fail test :: get 1 pins each frame should return 10

--- BowlingGame/BowlingGame/Bowling.cs
+++ BowlingGame/BowlingGame/Bowling.cs
@@ -5,13 +5,19 @@ namespace Bowling
     public class Bowling
     {
         readonly BowlingGame _bowlingGame = new BowlingGame();
+
         [Test]
         public void all_zero_pins_should_return_0()
         {
-            
-            //arrange 
             var playerResult = "0- 0- 0- 0- 0- 0- 0- 0- 0- 0-";
             Assert.AreEqual(0, _bowlingGame.GetScoreBy(playerResult));
+        }
+
+        [Test]
+        public void all_one_pins_should_return_10()
+        {
+            var playerResult = "1- 1- 1- 1- 1- 1- 1- 1- 1- 1-";
+            Assert.AreEqual(10, _bowlingGame.GetScoreBy(playerResult));
         }
     }
 }
