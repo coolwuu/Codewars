96e22e93d1fac00be234c7cc7ec29e1532d5fc07
coolwuu <coolwuu0207@gmail.com>
2017-11-12 21:16:30 +0800

    add fail test :: 0 pins return 0

--- BowlingGame/BowlingGame/Bowling.cs
+++ BowlingGame/BowlingGame/Bowling.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
-namespace BowlingGame
+namespace Bowling
 {
     public class Bowling
     {
+    
+        [Test]
+        public void all_zero_pins_should_return_0()
+        {
+            //arrange 
+            var playerResult = "0- 0- 0- 0- 0- 0- 0- 0- 0- 0-";
+            var bowlingGame = new BowlingGame();
+            Assert.AreEqual(0, bowlingGame.GetScoreBy(playerResult));
+        }
+    }
+
+    public class BowlingGame
+    {
+        public int GetScoreBy(string playerResult)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
