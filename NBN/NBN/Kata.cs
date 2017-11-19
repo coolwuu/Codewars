918166eaef1593149236f2ac45ecf00bf724827d
coolwuu <coolwuu0207@gmail.com>
2017-11-19 11:36:18 +0800

    add fail test : input 9 should return -1

--- NBN/NBN/Kata.cs
+++ NBN/NBN/Kata.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace NBN
 {
     public class Kata
     {
+        [Test]
+        public void input_8_should_return_minus_one()
+        {
+            Assert.AreEqual(-1,NextBiggerNumber(9));
+        }
+
+        private int NextBiggerNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
