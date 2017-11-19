f56c349e0af3076d19c5a13539627c855060576d
coolwuu <coolwuu0207@gmail.com>
2017-11-19 10:13:18 +0800

    add fail test :: input 9 should return -1

--- NextBiggerNumberWithSameDigits/NextBiggerNumberWithSameDigits/Kata.cs
+++ NextBiggerNumberWithSameDigits/NextBiggerNumberWithSameDigits/Kata.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace NextBiggerNumberWithSameDigits
 {
     public class Kata
     {
+        [Test]
+        public void input_9_should_return_minus_1()
+        {
+            Assert.AreEqual(-1,NextBigNumber(9));
+        }
+
+        private int NextBigNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
