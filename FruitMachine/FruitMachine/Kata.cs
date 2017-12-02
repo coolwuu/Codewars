55b91fbd021b230da4b651fa42fa4ca6568801fc
coolwuu <coolwuu0207@gmail.com>
2017-12-02 21:47:35 +0800

    add failed test

--- FruitMachine/FruitMachine/Kata.cs
+++ FruitMachine/FruitMachine/Kata.cs
@@ -6,12 +6,20 @@ namespace FruitMachine
     public class Kata
     {
         [Test]
-        public void No_matching_return_Zero()
+        public void No_matching_return_Zero_()
         {
             string[] reel = { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
             List<string[]> reels = new List<string[]> { reel, reel, reel };
             int[] spins = { 0, 1, 2 };
             Assert.AreEqual(0, Fruit(reels, spins));
+        }
+        [Test]
+        public void Two_of_Jack__return_1()
+        {
+            string[] reel = { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            List<string[]> reels = new List<string[]> { reel, reel, reel };
+            int[] spins = { 9, 9, 4 };
+            Assert.AreEqual(1, Fruit(reels, spins));
         }
 
         private static int Fruit(List<string[]> reels, int[] spins)
