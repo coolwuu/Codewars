0b073ddad11c3d9b4cb2913754189933e1ebdafd
coolwuu <coolwuu0207@gmail.com>
2017-12-02 21:54:51 +0800

    add failed test

--- FruitMachine/FruitMachine/Kata.cs
+++ FruitMachine/FruitMachine/Kata.cs
@@ -21,6 +21,14 @@ namespace FruitMachine
             int[] spins = { 9, 9, 4 };
             Assert.AreEqual(1, Fruit(reels, spins));
         }
+        [Test]
+        public void Three_of_Jack__return_10()
+        {
+            string[] reel = { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            List<string[]> reels = new List<string[]> { reel, reel, reel };
+            int[] spins = { 9, 9, 9 };
+            Assert.AreEqual(10, Fruit(reels, spins));
+        }
 
         private static int Fruit(List<string[]> reels, int[] spins)
         {
