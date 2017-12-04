4e1df1ec5c7c3d3ca2bf28fa356a35d042217529
coolwuu <coolwuu0207@gmail.com>
2017-12-04 21:29:27 +0800

    add test and pass

--- FruitMachine2/FruitMachine2/Kata.cs
+++ FruitMachine2/FruitMachine2/Kata.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace FruitMachine2
 {
     public class Kata
     {
+        [Test]
+        public void No_matching_items_return_Zero_()
+        {
+            string[] reel = { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            List<string[]> reels = new List<string[]> { reel, reel, reel };
+            int[] spins = { 0, 1, 2 };
+            Assert.AreEqual(0, Fruit(reels, spins));
+        }
+
+
+        private static int Fruit(List<string[]> reels,int[] spins)
+        {
+            return 0;
+        }
     }
 }
