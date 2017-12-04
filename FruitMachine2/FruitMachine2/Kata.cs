3df4fba86e40b676b6834f7a3883bb7b02d4d3fc
coolwuu <coolwuu0207@gmail.com>
2017-12-04 21:36:23 +0800

    add test and pass

--- FruitMachine2/FruitMachine2/Kata.cs
+++ FruitMachine2/FruitMachine2/Kata.cs
@@ -14,9 +14,24 @@ namespace FruitMachine2
             Assert.AreEqual(0, Fruit(reels, spins));
         }
 
+        [Test]
+        public void Two_of_Jack_return_1()
+        {
+            string[] reel = { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            List<string[]> reels = new List<string[]> { reel, reel, reel };
+            int[] spins = { 9, 9, 4 };
+            Assert.AreEqual(1, Fruit(reels, spins));
+        }
 
         private static int Fruit(List<string[]> reels,int[] spins)
         {
+            List<string> result = new List<string>();
+            for (int i = 0; i < reels.Count; i++)
+            {
+                result.Add(reels[i][spins[i]]);
+            }
+            if (result[0] == result[1] || result[1] == result[2])
+                return 1;
             return 0;
         }
     }
