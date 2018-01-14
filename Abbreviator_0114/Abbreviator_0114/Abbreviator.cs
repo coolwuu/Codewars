8cab008f6c23f3a4e5cc438a045d25e3948cefd8
coolwuu <coolwuu0207@gmail.com>
2018-01-14 20:33:17 +0800

    add new kata Abbreviator

---
+++ Abbreviator_0114/Abbreviator_0114/Abbreviator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abbreviator_0114
+{
+    public class Abbreviator
+    {
+    }
+}
