0e8331285984fad7f6c357691b3e50cd014982d2
coolwuu <coolwuu0207@gmail.com>
2018-01-14 21:02:55 +0800

    add test and pass

--- Abbreviator_0114/Abbreviator_0114/AbbreviatorTests.cs
+++ Abbreviator_0114/Abbreviator_0114/AbbreviatorTests.cs
@@ -12,7 +12,8 @@ namespace Abbreviator_0114
     {
         private readonly Abbreviator _abbreviator = new Abbreviator();
 
-        [TestCase("a", "a", TestName = "Return_input_when_length_equal_to_1")]
+        [TestCase("a","a",TestName = "Return_input_when_length_equal_to_1")]
+        [TestCase("an","an",TestName = "Return_input_when_length_equal_to_2")]
         [TestCase("ant","ant",TestName = "Return_input_when_length_equal_to_3")]
         public void Abbreviate(string expected, string input)
         {
