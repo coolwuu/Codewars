f1db0e706d760e70b53e3f11e11f258ad23a3675
coolwuu <coolwuu0207@gmail.com>
2018-01-14 20:55:30 +0800

    add failed test

---
+++ Abbreviator_0114/Abbreviator_0114/Abbreviator.cs
@@ -0,0 +1,10 @@
+﻿namespace Abbreviator_0114
+{
+    public class Abbreviator
+    {
+        public string Abbreviate(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Abbreviator_0114/Abbreviator_0114/AbbreviatorTests.cs
+++ Abbreviator_0114/Abbreviator_0114/AbbreviatorTests.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Abbreviator_0114
 {
+    [TestFixture]
     public class AbbreviatorTests
     {
+        private readonly Abbreviator _abbreviator = new Abbreviator();
 
+        [Test]
+        public void Abbreviate()
+        {
+            Assert.AreEqual("ant", _abbreviator.Abbreviate("ant"));
+        }
     }
 }
