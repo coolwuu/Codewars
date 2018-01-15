c1cb89eb8abec056aca6d68d36645fc23cba69d1
coolwuu <coolwuu0207@gmail.com>
2018-01-16 00:49:11 +0800

    add failed test

--- Abbreviator_0114/Abbreviator_0114/AbbreviatorTests.cs
+++ Abbreviator_0114/Abbreviator_0114/AbbreviatorTests.cs
@@ -19,6 +19,7 @@ namespace Abbreviator_0114
         [TestCase("m3y","money",TestName = "Return_input_when_length_equal_to_5")]
         [TestCase("m3y m4y","money monkey",TestName = "Return input when pass in 2 words")]
         [TestCase("m3y m4y m6s","money monkey memories",TestName = "Return input when pass in 3 words")]
+        [TestCase("e6t-r2e","elephant-ride",TestName = "Return input when pass in 2 words with special character")]
         public void Abbreviate(string expected, string input)
         {
             Assert.AreEqual(expected, _abbreviator.Abbreviate(input));
