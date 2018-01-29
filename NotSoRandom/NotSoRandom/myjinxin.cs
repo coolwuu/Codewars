44ea924415dfec3afea26bf4461b90e910749fd2
coolwuu <coolwuu0207@gmail.com>
2018-01-29 22:03:13 +0800

    change to test case

--- NotSoRandom/NotSoRandom/myjinxin.cs
+++ NotSoRandom/NotSoRandom/myjinxin.cs
@@ -10,12 +10,10 @@ namespace NotSoRandom
     [TestFixture]
     public class Myjinxin
     {
-        [Test]
-        public void one_black_one_white_should_return_black()
+        [TestCase(1,1,"Black")]
+        public void one_black_one_white_should_return_black(double black,double white,string expected)
         {
-            double black = 1;
-            double white = 1;
-            Assert.AreEqual("Black",NotSoRandom(black,white));
+            Assert.AreEqual("Black",NotSoRandom(black, white));
         }
 
         private static string NotSoRandom(double black, double white)
