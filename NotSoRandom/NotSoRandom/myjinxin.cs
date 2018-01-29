383c228abec066ad2c9cf37fe7525da30ba5066e
coolwuu <coolwuu0207@gmail.com>
2018-01-29 22:05:52 +0800

    add fail test 2 black 1 white return white

--- NotSoRandom/NotSoRandom/myjinxin.cs
+++ NotSoRandom/NotSoRandom/myjinxin.cs
@@ -10,10 +10,11 @@ namespace NotSoRandom
     [TestFixture]
     public class Myjinxin
     {
-        [TestCase(1,1,"Black")]
-        public void one_black_one_white_should_return_black(double black,double white,string expected)
+        [TestCase(1, 1, "Black", TestName = "one_black_one_white_should_return_black")]
+        [TestCase(2, 1, "White", TestName = "two_black_one_white_should_return_white")]
+        public void Test(double black, double white, string expected)
         {
-            Assert.AreEqual("Black",NotSoRandom(black, white));
+            Assert.AreEqual(expected, NotSoRandom(black, white));
         }
 
         private static string NotSoRandom(double black, double white)
