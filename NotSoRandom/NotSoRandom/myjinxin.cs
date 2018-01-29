e0e8c0c464110fd1ec27233b8c3f900b1c16ba49
coolwuu <coolwuu0207@gmail.com>
2018-01-29 22:00:28 +0800

    Add fail test 1 black and 1 white return black

--- NotSoRandom/NotSoRandom/myjinxin.cs
+++ NotSoRandom/NotSoRandom/myjinxin.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace NotSoRandom
 {
+    [TestFixture]
     public class Myjinxin
     {
+        [Test]
+        public void one_black_one_white_should_return_black()
+        {
+            double black = 1;
+            double white = 1;
+            Assert.AreEqual("Black",NotSoRandom(black,white));
+        }
+
+        private static string NotSoRandom(double black, double white)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
