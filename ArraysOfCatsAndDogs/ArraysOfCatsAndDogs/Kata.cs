10cac1ac3786dd1fef37aa9188cdeb63b71376ed
coolwuu <coolwuu0207@gmail.com>
2018-01-29 22:43:04 +0800

    add fail test

--- ArraysOfCatsAndDogs/ArraysOfCatsAndDogs/Kata.cs
+++ ArraysOfCatsAndDogs/ArraysOfCatsAndDogs/Kata.cs
@@ -10,6 +10,15 @@ namespace ArraysOfCatsAndDogs
     [TestFixture]
     public class Kata
     {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(1,Kata.Solve(new List<char>{'D','C'},1));
+        }
 
+        private static int Solve(List<char> pets, int steps)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
